0bec694b78d6030380766f94c367977f530fc25e
Dominique Karreman <s1169919@student.windesheim.nl>
2022-12-28 01:17:01 +0100

    migration change

--- database/migrations/2022_12_26_202034_create_players_table.php
+++ database/migrations/2022_12_26_202034_create_players_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('players', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('team_id')->references('id')->on('teams');
-            $table->foreignId('quiz_id')->references('id')->on('quizes');
-            $table->string('wrong');
-            $table->string('right');
+            $table->foreignId('team_id')->references('id')->on('teams')->nullable();
+            $table->foreignId('quiz_id')->references('id')->on('quizes')->nullable();
+            $table->string('wrong')->nullable();
+            $table->string('right')->nullable();
             $table->timestamps();
         });
     }
