3b4b6fb9157699972c3bbed094ef46618d9fa414
Dominique Karreman <s1169919@student.windesheim.nl>
2023-02-20 20:44:33 +0100

    fixed menu and made hamburger menu

--- public/js/fullpage.js
+++ public/js/fullpage.js
@@ -1,13 +1,42 @@
 
-new fullpage('#fullpage', {
+
+function scrollTo(sectionName){
+  console.log(sectionName)
+    switch(sectionName){
+        case 'Home':
+            fullPage.moveTo(1);
+            break;
+        case 'Work':
+            fullPage.moveTo(2);
+            break;
+        case 'About':
+            fullPage.moveTo(3);
+            break;
+        case 'Timeline':
+            fullPage.moveTo(4);
+            break;
+        case 'Gear':
+            fullPage.moveTo(5);
+            break;
+        case 'Hire':
+            fullPage.moveTo(6);
+            break;
+        default:
+            fullPage.moveTo(1);
+            break;
+    }
+
+}
+var fullPage = new fullpage('#fullpage', {
     // fullpage options go here
     licenseKey: 'asdaasdasdasdsd',
-    sectionsColor: ['#f2f2f2', '#1C1C1C', '#7BAABE'],
-    scrollingSpeed: 500,
-    autoScrolling: true,
+    sectionsColor: ['#1C1C1C', '#1C1C1C', '#1C1C1C', '#1C1C1C', '#1C1C1C', '#1C1C1C'],
+    scrollingSpeed: 700,
+    // autoScrolling: true,
+    // menu: '#menu',
     // parallax: true,
     navigation: false,
-    anchors: ['Home', 'About', 'Skills'],
+    
 
     afterLoad: function(origin, destination, direction) {
       // Store the active section in a variable
@@ -15,7 +44,11 @@ new fullpage('#fullpage', {
         index: destination.index,
         element: destination.item
       };
-  
+      if(activeSection.index !== 0){
+        document.getElementById('top').style.opacity = '100%';
+      } else {
+        document.getElementById('top').style.opacity = '0%';
+      }
       // Do something with the active section variable
       console.log(activeSection);
     }
@@ -23,3 +56,26 @@ new fullpage('#fullpage', {
     // showActiveTooltip: true,
   });
   
+  let hamburger = document.getElementById('hamburger');
+  let navMD = document.getElementById('navbar-cta');
+  hamburger.addEventListener('click', function() {
+    // hamburger.style.height = '100%';
+    if (navMD.style.height == '100vh') {
+      navMD.style.height = '0';
+      navMD.style.opacity = '0%';
+    } else {
+    navMD.style.height = '100vh';
+    navMD.style.opacity = '100%';
+    }
+  });
+
+  let menu = document.querySelectorAll('.menuButton');
+
+  let menuIndex = 0;
+for(const menuItem of menu) {
+  
+  menuItem.addEventListener('click', () => {scrollTo(menuItem.innerHTML)})
+
+}
+let topButton = document.getElementById('top');
+topButton.addEventListener('click', () => {scrollTo('Home')})
--- resources/views/photography/home.blade.php
+++ resources/views/photography/home.blade.php
@@ -45,50 +45,89 @@
 </head>
 
 <body>
+    <div id="backToTop"  class="backToTop">
+        <button style="opacity: 0%; transition: 1s;" id="top">Back to top</button>
+    </div>
     <div id="fullpage">
-        <div id="main" class="section s1">
+        <div id="Home" class="section s1">
             <div class="topBar flex flex-row w-full h-[10vh]  absolute inset-0 justify-between">
                 <img class="logo h-[15vh]" src="{{ asset('images/logo.svg') }}" alt="">
-                <a class="m-5  text-white customBorder" href="#">Contact</a>
+                <a class="m-5  hidden md:flex text-white customBorder" href="#">Contact</a>
+                <div class="flex md:hidden">
+                    <button id="hamburger" type="button"
+                        class="z-20 inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden dark:text-gray-400">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-8 h-8" aria-hidden="true" fill="white" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <div id="navbar-cta" style="opacity: 0%; height: 0px;" class=" navMD">
+                    <ul>
+                        <li>
+                           <a  class="text-white text-[5vh]" href="#">Contact</a> 
+                        </li>
+                      
+                    </ul>
+                </div>
             </div>
-            <div class="hero">
+            <div class="hidden sm:flex hero">
                 <span>
                     <h1>DK Visuals</h1>
                     <h1>Photography</h1>
                 </span>
             </div>
-            <div id="menu" class="menu flex h-[50vh] w-[80vh] float-right justify-end text-right mr-5">
-                <ul class="flex justify-between flex-row flex-wrap text-right ">
+            <div class="heroSM sm:hidden mr-5">
+                <span>
+                    <h1>DK Visuals</h1>
+                    {{-- <h1>Photography</h1> --}}
+                </span>
+            </div>
+            <div  class="menu flex h-[50vh] sm:h-[45vh] w-[80vh] float-right justify-end text-right mr-5">
+                <ul id="menu" class="flex justify-between flex-row flex-wrap text-right ">
                     <li data-menuanchor="Home"
-                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><a
-                            data-text="Home" class=" text-[6vh] drop" href="#main">Home</a></li>
+                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><button 
+                            data-text="Home" class="menuButton  text-[6vh] drop">Home</button></li>
                     <li
                         data-menuanchor="About"class="h-[5vh] flex items-center w-full text-right justify-end place-content-end">
-                        <a data-text="Work" class="text-[6vh] drop" href="#2">Work</a>
+                        <button data-text="Work" class="menuButton text-[6vh] drop">Work</button>
                     </li>
                     <li data-menuanchor="Skills"
-                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><a
-                            data-text="About" class="text-[6vh] drop" href="#3">About</a></li>
+                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><button 
+                            data-text="About" class="menuButton text-[6vh] drop">About</button></li>
                     <li data-menuanchor="Skills"
-                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><a
-                            data-text="Timeline" class="text-[6vh] drop" href="#3">Timeline</a></li>
+                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><button 
+                            data-text="Timeline" class="menuButton text-[6vh] drop">Timeline</button></li>
                     <li data-menuanchor="Skills"
-                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><a
-                            data-text="Gear" class="text-[6vh] drop" href="#3">Gear</a></li>
+                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><button 
+                            data-text="Gear" class="menuButton text-[6vh] drop">Gear</button></li>
                     <li data-menuanchor="Skills"
-                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><a
-                            data-text="Hire" class="text-[6vh] drop" href="#3">Hire</a></li>
+                        class="h-[5vh] relative flex items-center w-full text-right justify-end place-content-end"><button 
+                            data-text="Hire" class="menuButton text-[6vh] drop">Hire</button></li>
 
                 </ul>
             </div>
         </div>
-        <div id="2" class="section s2 flex center midd">
-            <h1 class="text-white text-xl text-bold middtext">Section 2</h1>
+        <div id="Work" class="section s2 flex center midd">
+            <h1 class="text-white text-xl text-bold middtext">Work</h1>
         </div>
-        <div id="3" class="section s3">
-            <h1>Section 3</h1>
+        <div id="About" class="section s3">
+            <h1>About</h1>
+        </div>
+        <div id="Timeline" class="section s4">
+            <h1>Timeline</h1>
+        </div>
+        <div id="Gear" class="section s5">
+            <h1>Gear</h1>
+        </div>
+        <div id="Hire" class="section s6">
+            <h1>Hire</h1>
         </div>
     </div>
     <script src="{{ asset('js/fullpage.js') }}"></script>
 </body>
+
 </html>
