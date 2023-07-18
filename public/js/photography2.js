7da81f698c4ab74f137fe1b370d37ee21925ebe6
Dominique Karreman <s1169919@student.windesheim.nl>
2023-07-18 14:16:15 +0200

    updated website

--- public/js/photography2.js
+++ public/js/photography2.js
@@ -1,90 +1,169 @@
-const imageTrackBackground = document.querySelector('#trackBackground');
-const imageTrack = document.querySelector('#image-track');
-const overlay = document.querySelector('#overlay-hero');
-const recentTitle = document.querySelector('#recentTitle');
+const imageTrackBackground = document.querySelector("#trackBackground");
+const imageTrack = document.querySelector("#image-track");
+const overlay = document.querySelector("#overlay-hero");
+const recentTitle = document.querySelector("#recentTitle");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll("nav a");
 
-const backgroundDiv = document.querySelector('#herooDiv');
+const backgroundDiv = document.querySelector("#herooDiv");
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-        console.log('Image track is visible!');
-        backgroundDiv.style.backgroundSize = '100%';
-        imageTrack.style.transform = "translate(0%, 0)";
-        backgroundDiv.style.backgroundPosition = "center 5vh";
-        overlay.style.opacity = 0.45;
-        recentTitle.style.opacity = 1;
-        recentTitle.style.transform = "translate(0, 0)";
-      } else {
-        backgroundDiv.style.backgroundSize = '140%';
-        imageTrack.style.transform = "translate(50%, 0)";
-        overlay.style.opacity = 0;
-        backgroundDiv.style.backgroundPosition = "center -10vh";
-        recentTitle.style.opacity = 0;
-        recentTitle.style.transform = "translateY(10vh)";
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log("Image track is visible!");
+            backgroundDiv.style.backgroundSize = "100%";
+            imageTrack.style.transform = "translate(0%, 0)";
+            imageTrackBackground.style.transform = "translate(0%, 0)";
+            backgroundDiv.style.backgroundPosition = "center 5vh";
+            overlay.style.opacity = 0.45;
+            recentTitle.style.opacity = 1;
+            imageTrack.style.opacity = 1;
 
-      }
-  });
-});
+            recentTitle.style.transform = "translate(0, 0)";
+        } else {
+
+
+
+            backgroundDiv.style.backgroundSize = "140%";
+
+            overlay.style.opacity = 0;
+            backgroundDiv.style.backgroundPosition = "center -10vh";
+        }
+    });
+},{ threshold: 0.5 });
 
 observer.observe(recentTitle);
-
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            backgroundDiv.style.backgroundSize = "140%";
+            imageTrack.style.transform = "translate(50%, 0)";
+            overlay.style.opacity = 0;
+            backgroundDiv.style.backgroundPosition = "center -10vh";
+            recentTitle.style.opacity = 0;
+            recentTitle.style.transform = "translateY(10vh)";
+            imageTrack.style.transform = "translate(50%, 0)";
+            imageTrack.style.opacity = 0;
+            recentTitle.style.opacity = 0;
+            recentTitle.style.transform = "translateY(10vh)";
+        } else {
+        }
+    });
+});
+observer2.observe(nav);
 
 const track = document.getElementById("image-track");
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
 const handleOnUp = () => {
     track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
-}
+};
 
-const handleOnMove = e => {
+const handleOnMove = (e) => {
     if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+        nextPercentageUnconstrained =
+            parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(
+            Math.min(nextPercentageUnconstrained, 0),
+            -100
+        );
 
     track.dataset.percentage = nextPercentage;
 
-    track.animate({
-        transform: `translate(${nextPercentage}%)`
-    }, {
-        duration: 1200,
-        fill: "forwards"
-    });
+    track.animate(
+        {
+            transform: `translate(${nextPercentage}%)`,
+        },
+        {
+            duration: 1200,
+            fill: "forwards",
+        }
+    );
 
     for (const image of track.getElementsByClassName("image")) {
-        image.animate({
-            objectPosition: `${100 + nextPercentage}% `
-        }, {
-            duration: 1200,
-            fill: "forwards"
-        });
+        image.animate(
+            {
+                objectPosition: `${100 + nextPercentage}% `,
+            },
+            {
+                duration: 1200,
+                fill: "forwards",
+            }
+        );
     }
-}
+};
 
 /* -- Had to add extra lines for touch events -- */
 
-window.onmousedown = e => handleOnDown(e);
+window.onmousedown = (e) => handleOnDown(e);
 
-window.ontouchstart = e => handleOnDown(e.touches[0]);
+window.ontouchstart = (e) => handleOnDown(e.touches[0]);
 
-window.onmouseup = e => handleOnUp(e);
+window.onmouseup = (e) => handleOnUp(e);
 
-window.ontouchend = e => handleOnUp(e.touches[0]);
+window.ontouchend = (e) => handleOnUp(e.touches[0]);
 
-window.onmousemove = e => handleOnMove(e);
+window.onmousemove = (e) => handleOnMove(e);
 
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+window.ontouchmove = (e) => handleOnMove(e.touches[0]);
 
 window.onload = () => {
-    const body = document.querySelector('body');
-    const name = document.querySelector('#nameTekening');
-    name.style.opacity = '1';
-    name.style.transform = 'translate(0, 0)';
-    body.style.opacity = '1';
-  };
+    const body = document.querySelector("body");
+    const name = document.querySelector("#nameTekening");
+    name.style.opacity = "1";
+    name.style.transform = "translate(0, 0)";
+    body.style.opacity = "1";
+};
+
+const activityDiv = document.querySelector("#activityDiv");
+const activitySubs = document.getElementsByClassName("activitySub");
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            activitySubs[0].style.transform = "translate(0, 0)";
+            activitySubs[1].style.transform = "translate(0, 0)";
+            activitySubs[2].style.transform = "translate(0, 0)";
+            activitySubs[0].style.opacity = "1";
+            activitySubs[1].style.opacity = "1";
+            activitySubs[2].style.opacity = "1";
+
+        } else {
+        }
+    });
+});
+
+observer3.observe(activitySubs[0]);
+observer3.observe(activitySubs[1]);
+observer3.observe(activitySubs[2]);
+
+const observerNav = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+
+                navLinks.forEach(element => {
+                    element.style.color = "white";
+                 });
+
+
+        } else {
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                navLinks.forEach(element => {
+                    element.style.color = "white";
+                 });
+                //  nav.classList.remove("blur2");
+              } else {
+                navLinks.forEach(element => {
+                    element.style.color = "black";
+                    // nav.classList.add("blur2");
+                 });
+              }
+        }
+    });},{ threshold: 0.5 });
+    const name = document.querySelector("#nameTekening");
+    observerNav.observe(name);
--- resources/views/photography/home2.blade.php
+++ resources/views/photography/home2.blade.php
@@ -9,7 +9,7 @@
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
         {{-- <link rel="stylesheet" href="css/nav.css"> --}}
-        <link rel="stylesheet" href="/css/full.css">
+        {{-- <link rel="stylesheet" href="/css/full.css"> --}}
 
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
@@ -46,29 +46,29 @@
         </script>
     </head>
 
-    <body style="opacity: 0 transition: 2s ease" class="bg-white dark:bg-zinc-900">
-        <nav class="flex shift justify-between flex-row bg-transperent p-5 w-full h-[10vh] absolute z-10">
+    <body style="opacity: 0 transition: 2s ease" class="bg-white dark:bg-black">
+        <nav class="flex shift justify-between flex-row bg-transperent p-5 w-full h-[10vh] fixed z-10">
             <a href=""><img src="/images/logo.svg" class="w-[10vh] relative left-[5vw]" alt=""></a>
 
             <ul class="flex gap-2 z-10 mr-3 bg-transparent">
                 <li class="overflow-hidden2">
-                    <a class="drop-in-4 p-2 font-sans text-white italic text-[2rem] font-black"
+                    <a class="drop-in-4 p-2 font-sans text-white italic text-[2rem] navItem font-black"
                         style="font-family: 'Roboto Condensed', sans-serif;" href="/">Home</a>
                 </li>
                 <li class="overflow-hidden2">
-                    <a class="drop-in-5 p-2 font-sans text-white italic text-[2rem] font-black"
+                    <a class="drop-in-5 p-2 font-sans text-white italic text-[2rem] navItem font-black"
                         style="font-family: 'Roboto Condensed', sans-serif;" href="/">Portfolio</a>
                 </li>
                 <li class="overflow-hidden2">
-                    <a class="font-sans drop-in-6 p-2 text-white italic text-[2rem] font-black"
+                    <a class="font-sans drop-in-6 p-2 text-white italic text-[2rem] navItem font-black"
                         style="font-family: 'Roboto Condensed', sans-serif;" href="/">Gear</a>
                 </li>
                 <li class="overflow-hidden2">
-                    <a class="font-sans text-white drop-in-7 p-2 italic text-[2rem] font-black"
+                    <a class="font-sans text-white drop-in-7 p-2 italic text-[2rem] navItem font-black"
                         style="font-family: 'Roboto Condensed', sans-serif;" href="/">About</a>
                 </li>
                 <li class="overflow-hidden2">
-                    <a class="font-sans text-white italic drop-in-8 p-2 text-[2rem] font-black"
+                    <a class="font-sans text-white italic drop-in-8 p-2 text-[2rem] navItem font-black"
                         style="font-family: 'Roboto Condensed', sans-serif;" href="/">Contact</a>
                 </li>
             </ul>
@@ -77,12 +77,13 @@
 
         <div id="herooDiv"
             style="background-image: url('/images/photography2.jpg'); background-repeat: no-repeat; transition: 1s ease;"
-            class="w-full  h-[100vh]">
+            class="w-full  overflow-y-hidden h-[100vh]">
 
             <div style="transition: 1.5s ease;" id="overlay-hero"
-                class="absolute inset-0 h-[100vh] bg-black opacity-[0.45]">
+                class="relative  inset-0 h-[100vh] bg-black opacity-[0.45]">
             </div>
-            <div class="absolute justify-center align-center h-[100vh] left-[10vw] flex flex-col gap-2 m-0">
+            <div
+                class="relative w-[80vw] top-[-95vh] justify-center align-center h-[90vh] left-[10vw] flex flex-col gap-2 m-0">
 
 
                 <div class="overflow-hidden2">
@@ -104,25 +105,26 @@
                         Photo/Videographer
                     </h1>
                 </div>
+                <div class="overflow-hidden2 mt-[20vh] mb-[-30vh]">
+                    <div id="nameTekening" class=" flex flex-col items-center justify-center top-[60vh]"
+                        style="opacity: 0; transform:translateY(10vh); transition: 2s ease">
 
+                        <img src="/images/handtekening.png" class=" drop-in-9 bottom-[5vh] w-[15vh]" alt="">
+                        <h1 id="name" class="drop-in-9 text-white text-[1.5rem] font-black italic">DOMINIQUE
+                            KARREMAN</h1>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="overflow-hidden2">
-            <div id="nameTekening" class="absolute w-[100vw] flex flex-col items-center justify-center top-[90vh]"
-                style="opacity: 0; transform:translateY(10vh); transition: 2s ease">
 
-                <img src="/images/handtekening.png" class="absolute drop-in-9 bottom-[5vh] w-[15vh]" alt="">
-                <h1 id="name" class="drop-in-9 text-white text-[1.5rem] font-black absolute italic">DOMINIQUE
-                    KARREMAN</h1>
-            </div>
         </div>
+
         </div>
-        <div id="trackBackground" class="">
+        <div id="trackBackground" class="bg-zinc-300 pb-[3vh] dark:bg-zinc-900">
             <h1 id="recentTitle" style="transition: 1.5s ease; opacity: 0; transform: translateY(10vh)"
                 class="font-black text-[2rem] text-black italic p-[3vh] dark:text-white">RECENT WORK</h1>
             {{-- <h1 class="text-white text-xl text-bold middtext">Work</h1> --}}
-            <div id="image-track" style="transition: 1.5s ease; transform: translate(50%, 0)" class="mb-[10vh] "
-                data-mouse-down-at="0" data-prev-percentage="0">
+            <div id="image-track" style="transition: 1.5s ease; transform: translate(50%, 0); opacity: 0;"
+                class="mb-[10vh] " data-mouse-down-at="0" data-prev-percentage="0">
                 <img class="image ml-5" src="/images/DSC03824.jpg" draggable="false" />
                 <img class="image" src="/images/esra.jpg" draggable="false" />
                 <img class="image" src="/images/IMG_9954.png" draggable="false" />
@@ -134,12 +136,90 @@
             </div>
 
         </div>
-        <div class="bg-black relative w-[100vw] h-[100vh]">
+        <div id="activityDiv"
+            class="bg-white dark:bg-black relative  gap-[5vh] flex flex-row   justify-around p-[5vh] pt-[5vh]  relative w-[100vw] h-[100vh]">
+            <div style="opacity: 0; transition: 1s ease; transform: translateY(10vh)"
+                class="activitySub flex flex-col items-center align-center  w-1/3">
+                <svg class="hidden dark:flex" xmlns="http://www.w3.org/2000/svg" height="48" fill="white"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M454-602h345q-28-74-89-130.5T571-810L454-602Zm-93 83 174-299q-12-1-27-1.5t-28-.5q-72 0-132 26t-108 72l121 203ZM150-393h237L217-696q-38 45-57.5 100.5T140-480q0 21 2 44t8 43Zm240 242 120-207H162q28 74 88.5 130.5T390-151Zm90 11q72 0 132.5-26T720-238L600-441 425-142q13 1 27.5 1.5t27.5.5Zm264-124q34-42 55-99t21-117q0-22-2-44.5t-7-43.5H574l170 304ZM480-480Zm0 400q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z" />
+                </svg>
+                <svg class="dark:hidden" xmlns="http://www.w3.org/2000/svg" height="48" fill="black"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M454-602h345q-28-74-89-130.5T571-810L454-602Zm-93 83 174-299q-12-1-27-1.5t-28-.5q-72 0-132 26t-108 72l121 203ZM150-393h237L217-696q-38 45-57.5 100.5T140-480q0 21 2 44t8 43Zm240 242 120-207H162q28 74 88.5 130.5T390-151Zm90 11q72 0 132.5-26T720-238L600-441 425-142q13 1 27.5 1.5t27.5.5Zm264-124q34-42 55-99t21-117q0-22-2-44.5t-7-43.5H574l170 304ZM480-480Zm0 400q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z" />
+                </svg>
+                <h1
+                    class=" flex flex-col text-black text-center align-center justify-center dark:text-white font-black text-[2rem]  italic">
+                    Photography</h1>
+                <p class="text-black dark:text-white text-center">Lorem ipsum
+                    dolor sit amet, consectetur adipiscing
+                    elit. Proin a lacinia nisl, a
+                    viverra felis. Proin
+                    libero libero, imperdiet dignissim mauris id, iaculis laoreet arcu. Nam eu tempus dui. Sed fermentum
+                    convallis fermentum. Integer luctus lacus quis odio faucibus, ac bibendum nisl condimentum.
+                    Pellentesque a justo pretium, posuere diam eget, vestibulum ligula. Nulla facilisi. Curabitur in
+                    lacus sapien. Sed efficitur arcu et enim cursus, vitae volutpat nibh sollicitudin. Quisque a odio
+                </p>
+            </div>
+            <div style="opacity: 0; transition: 1s ease; transform: translateY(10vh)"
+                class="activitySub flex flex-col items-center align-center  w-1/3">
+                <svg class="hidden dark:flex" xmlns="http://www.w3.org/2000/svg" fill="white" height="48"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M140-160q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h520q24 0 42 18t18 42v215l160-160v410L720-435v215q0 24-18 42t-42 18H140Zm0-60h520v-520H140v520Zm0 0v-520 520Z" />
+                </svg>
+                <svg class="dark:hidden" xmlns="http://www.w3.org/2000/svg" fill="black" height="48"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M140-160q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h520q24 0 42 18t18 42v215l160-160v410L720-435v215q0 24-18 42t-42 18H140Zm0-60h520v-520H140v520Zm0 0v-520 520Z" />
+                </svg>
+                <h1
+                    class=" flex flex-col text-black text-center align-center justify-center dark:text-white font-black text-[2rem]  italic">
+                    Videographer</h1>
+                <p class="text-black dark:text-white text-center">Lorem ipsum
+                    dolor sit amet, consectetur adipiscing
+                    elit. Proin a lacinia nisl, a
+                    viverra felis. Proin
+                    libero libero, imperdiet dignissim mauris id, iaculis laoreet arcu. Nam eu tempus dui. Sed fermentum
+                    convallis fermentum. Integer luctus lacus quis odio faucibus, ac bibendum nisl condimentum.
+                    Pellentesque a justo pretium, posuere diam eget, vestibulum ligula. Nulla facilisi. Curabitur in
+                    lacus sapien. Sed efficitur arcu et enim cursus, vitae volutpat nibh sollicitudin. Quisque a odio
+                </p>
+            </div>
+            <div style="opacity: 0; transition: 1s ease; transform: translateY(10vh)"
+                class="activitySub flex flex-col items-center align-center  w-1/3">
+                <svg class="hidden dark:flex" fill="white" xmlns="http://www.w3.org/2000/svg" height="48"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M140-200q-24 0-42-18t-18-42v-520q0-24 18-42t42-18l74 152h130l-74-152h89l74 152h130l-74-152h89l74 152h130l-74-152h112q24 0 42 18t18 42v208h-60v-56H140v368h368v60H140Zm604-243 28 28-164 164v51h51l164-164 28 28-176 176H568v-107l176-176Zm107 107L744-443l61-61q9-9 21-9t21 9l65 65q9 9 9 21t-9 21l-61 61Z" />
+                </svg>
+                <svg class="dark:hidden" fill="black" xmlns="http://www.w3.org/2000/svg" height="48"
+                    viewBox="0 -960 960 960" width="48">
+                    <path
+                        d="M140-200q-24 0-42-18t-18-42v-520q0-24 18-42t42-18l74 152h130l-74-152h89l74 152h130l-74-152h89l74 152h130l-74-152h112q24 0 42 18t18 42v208h-60v-56H140v368h368v60H140Zm604-243 28 28-164 164v51h51l164-164 28 28-176 176H568v-107l176-176Zm107 107L744-443l61-61q9-9 21-9t21 9l65 65q9 9 9 21t-9 21l-61 61Z" />
+                </svg>
+                <h1
+                    class=" flex flex-col text-black text-center align-center justify-center dark:text-white font-black text-[2rem]  italic">
+                    Video/Photo editing</h1>
+                <p class="text-black dark:text-white text-center">Lorem ipsum
+                    dolor sit amet, consectetur adipiscing
+                    elit. Proin a lacinia nisl, a
+                    viverra felis. Proin
+                    libero libero, imperdiet dignissim mauris id, iaculis laoreet arcu. Nam eu tempus dui. Sed fermentum
+                    convallis fermentum. Integer luctus lacus quis odio faucibus, ac bibendum nisl condimentum.
+                    Pellentesque a justo pretium, posuere diam eget, vestibulum ligula. Nulla facilisi. Curabitur in
+                    lacus sapien. Sed efficitur arcu et enim cursus, vitae volutpat nibh sollicitudin. Quisque a odio
+                </p>
+            </div>
 
         </div>
 
         <script src="/js/photography2.js"></script>
-
     </body>
+    <footer class="absolute self-center top-[300vh]">
+        <h1 class="text-white z-50 text-[55vh]">test</h1>
+    </footer>
 
     </html>
